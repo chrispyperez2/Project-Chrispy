7385dd0df8926f86c14fbe263c7fc3ba932a494f
chrispyperez2 <chris.perezca@gmail.com>
2023-09-06 17:56:50 -0700

    Added Additional Pages and routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import NavBar from './components/NavBar.js';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
     <NavBar/>
       <Routes>
         <Route path='/' exact Component={Home}/>
+        <Route path='/services' exact Component={Services}/>
+        <Route path='/products' exact Component={Products}/>
+        <Route path='/sign-up' exact Component={SignUp}/>
       </Routes>
    </Router>
     
