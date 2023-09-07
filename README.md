3ac91e4861e8a29a9614def7a9c20210ce38b5cd
chrispyperez2 <chris.perezca@gmail.com>
2023-09-07 00:45:42 -0700

    updated readme with todo list and removed subscribe email form

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Getting Started with Create React App
 
 This Project is Dedicated to representing who I am as a person and an interactive format of a resume for employment opportunties. 
+
+## TODO list:
+* Offload large MP4 files to s3 bucket and source from there.
+* add content to pages to explain who i am and what I am skilled at
+* discuss my travels
+* discuess my bond to my family and dogs
+* discuss what hobbies i enjoy
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,7 @@ function Footer() {
         <p className='footer-subscription-text'>
           You can unsubscribe at anytime.
         </p>
-        <div className='input-areas'>
-          <form>
-            <input type='email' name='email' placeholder='Your Email'/>
-            <Button buttonStyle='btn--outline'> Subscribe</Button>
-          </form>
-        </div>
+
       </section>
 
       <div className='footer-links'>
