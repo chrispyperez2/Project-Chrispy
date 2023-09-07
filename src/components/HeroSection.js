edfc76ffb444793bc8569f43ac18f4755d7d4ccc
chrispyperez2 <chris.perezca@gmail.com>
2023-09-07 00:36:44 -0700

    changed catch phrase

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container'>
       <video src="/videos/wrx.mp4" autoPlay loop muted />
       <h1>Christian Perez Castanaza</h1> 
-      <p>Who is he?</p>
+      <p>Racing to succeed in anything he does!</p>
       <div className='hero-btns'>
         <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large'>
           Why Hire him?
