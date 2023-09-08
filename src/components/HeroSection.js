e0df700e18761845c66c972a9510763b0bf71e1b
chrispyperez2 <chris.perezca@gmail.com>
2023-09-07 23:25:31 -0700

    added enye to name

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src="/videos/wrx.mp4" autoPlay loop muted />
-      <h1>Christian Perez Castanaza</h1> 
+      <h1>Christian Perez Castañaza</h1> 
       <p>Racing to succeed in EVERYTHING he does!</p>
       <div className='hero-btns'>
         <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large'>
