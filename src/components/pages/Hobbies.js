19f4226e4866776172cc5d2a5ede0703dd926e94
chrispyperez2 <chris.perezca@gmail.com>
2023-09-14 00:47:09 -0700

    removed duplicated project, added extra instagram module for aqua240 for hobbies

--- src/components/pages/Hobbies.js
+++ src/components/pages/Hobbies.js
@@ -10,6 +10,9 @@ export default function Hobbies() {
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <InstagramEmbed url="https://www.instagram.com/p/ChYjs0zL0Hh/?img_index=1" width={1000} />
       </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <InstagramEmbed url="https://www.instagram.com/p/Cl4k0RovZp3/?img_index=1" width={1000} />
+      </div>
     </div>
     <Footer/>
     </>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -23,12 +23,6 @@ export default function Projects() {
           src="https://d1hdl6pwuskfno.cloudfront.net/images/wrx.jpg"
           />
 
-          <ProjectItem
-          label="WRX Track Car / Volvo Drift Car"
-          text="Using my WRX and Volvo to teach myself automotive skills to eventually turn the 2020 WRX into a dedicated track car and the 1992 Volvo 240 into a dedicated drift car. The WRX has upgraded suspension and performance parts to push the car to its limits.
-          The Volvo has been relagated to the daily while I build up funds for an eventual engine swap. The current plan is an LM7 from an 1999-2007 Chevy Silverado. Currently out of money XD. You can follow the build progress of each car on their dedicated instagrams. 'Kuhrisp_WRX' and 'aqua_240'! "
-          src="https://d1hdl6pwuskfno.cloudfront.net/images/wrx.jpg"
-          />
         </ul>
       </div>
       <Footer/>
